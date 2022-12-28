97183e96825a6e9cbd803560cf5fecc6eddaacf2
[Narmin Bagirova] <[narminnbagirova@gmail.com]>
2022-12-28 19:03:47 +0400

    Add project files.

---
+++ algorithm task2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace algorithm_task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ededi daxil edin:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0, r, place = 1;
+            place = number.ToString().Length;
+
+
+            while (number != 0)
+            {
+                r = number % 10;
+                sum += r * place;
+                place--;
+                number /= 10;
+
+
+
+            }
+            Console.WriteLine(sum.ToString());
+        }
+    }
+}
